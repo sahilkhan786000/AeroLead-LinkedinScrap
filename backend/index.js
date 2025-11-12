2e6d0c69d55aceb702d8849d262da83a46d8cf9b
Sahil Khan <salmanengis6397@gmail.com>
2025-11-13 01:31:04 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -2,28 +2,24 @@ import express from "express";
 import cors from "cors";
 import { scrapeForMultipleUsers } from "./scraper.js"
 import path from "path";
-import { fileURLToPath } from "url";
-
-// ✅ Fix for __dirname and __filename in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
-// Serve frontend build files
-app.use(express.static(path.join(__dirname, "build" )));
+
+app.use(express.static(path.join(__dirname, "build")));
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
-
 
 
 app.post("/api/multi-scrape", async (req, res) => {
   try {
-    const { users } = req.body;
+    const {users}  = req.body;
     if (!users || !Array.isArray(users)) {
       return res.status(400).json({ error: "Invalid user data" });
     }
@@ -36,4 +32,4 @@ app.post("/api/multi-scrape", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+app.listen(PORT, () => console.log(" Backend running on http://localhost:5000"));
